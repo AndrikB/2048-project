3f5c1ca346df8892cbb3c157552d75946991e8bd
AndrikB <blagij00@gmail.com>
2019-05-12 23:32:36 +0300

    started adding gameWidget
    add generation items, check game_over

--- 2048/gamewidget.cpp
+++ 2048/gamewidget.cpp
@@ -1,9 +1,77 @@
 #include "gamewidget.h"
 #include <QDebug>
 
-gameWidget::gameWidget(int width, int heigth, QWidget *parent) : QWidget(parent)
+gameWidget::gameWidget(int width, int height, QWidget *parent) : QWidget(parent)
 {
-    elements.clear();
-    elements.fill(QVector<int>(width, 0), heigth);
+    //elements.clear();
+    elements.fill(QVector<qint8>(width, 0), height);
+
+    add_new_element();add_new_element();
+
+}
+
+bool gameWidget::game_over() const
+{
+    int heigth=elements.size();
+    int width=elements[0].size();
+    for (int i = 0; i < heigth; ++i)
+        for (int j=0;j<width;j++)
+            if (elements.at(i).at(j)==0)
+                return false;
+
+    for(int i = 0; i < heigth-1; i++){
+        for(int j = 0; j < width-1; j++){
+            if(elements[i][j] == elements[i+1][j] || elements[i][j] == elements[i][j+1])
+                return false;
+        }
+    }
+    for(int i = 0; i < heigth-1; i++){
+        if(elements[i][3] == elements[i+1][3])
+            return false;
+    }
+    for(int j = 0; j < width; j++){
+        if(elements[3][j] == elements[3][j+1])
+            return false;
+    }
+
+
+    return true;
+}
+
+void gameWidget::end_game()
+{
+    //todo
+}
+
+void gameWidget::add_new_element()
+{
+    QPoint pos=random_free_cell();
+    if (pos.x()==-1) return;
+    elements[pos.y()][pos.x()]=qrand()%2+1;
+}
+
+QPoint gameWidget::random_free_cell() const
+{
+    int heigth=elements.size();
+    int width=elements[0].size();
+    QVector<QPoint> free_pos;
+    for (int i = 0; i < heigth; ++i)
+        for (int j=0;j<width;j++)
+            if (elements.at(i).at(j)==0)
+                free_pos.push_back(QPoint(j,i));
+    if (free_pos.size()==0) {qDebug()<<"bug; isnt free cells"; return QPoint(-1,-1);}
+
+    return free_pos[qrand()%free_pos.size()];
+}
+
+void gameWidget::move(gameWidget::Move direction)
+{
+    //todo
+
+    add_new_element();
+
+    if (game_over()){end_game(); return;}
+
+    this->update();//todo update
 
 }
--- 2048/gamewidget.h
+++ 2048/gamewidget.h
@@ -7,14 +7,31 @@ class gameWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit gameWidget(int width, int height, QWidget *parent = nullptr);
+    explicit gameWidget(int width, int heigth, QWidget *parent);
+
+    enum Move{
+        right,
+        bottom,
+        left,
+        top
+    };
 
 signals:
+    void game_over(int score);
+    void update_score(int score);
 
 public slots:
 
 private:
-    QVector<QVector<int>> elements;
+    QVector<QVector<qint8>> elements;//zero == null
+    int score=0;
+
+    bool game_over() const;
+    void end_game();
+    void add_new_element();
+    QPoint random_free_cell() const;
+    void move(Move direction);
+
 };
 
 #endif // WIDGET_H
